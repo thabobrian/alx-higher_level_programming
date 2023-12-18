75b1b11aa5687842cee846ffac4c43151bc1b302
thabobrian <thabodhlomo05@gmail.com>
2023-12-18 01:30:10 -0800

    0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    total = 0
+    for i in range(e):
+        try:
+            print("{}".format(my_list[i], end="")
+                    total += 1
+                    expect IndexError:
+                    break
+                    print("")
+                    return (total)
